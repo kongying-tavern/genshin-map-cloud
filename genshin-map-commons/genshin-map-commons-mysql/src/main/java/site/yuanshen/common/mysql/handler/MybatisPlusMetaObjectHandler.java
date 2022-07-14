79919810f83ee267fbd1c22bc742caf732b80013
Moment <momincong@foxmail.com>
2022-07-14 22:44:03 +0800

    fix bug: creatorId and updaterId wrong insert；修复写入者和修改者id字段的错误生成写入

--- genshin-map-commons/genshin-map-commons-mysql/src/main/java/site/yuanshen/common/mysql/handler/MybatisPlusMetaObjectHandler.java
+++ genshin-map-commons/genshin-map-commons-mysql/src/main/java/site/yuanshen/common/mysql/handler/MybatisPlusMetaObjectHandler.java
@@ -31,7 +31,7 @@ public class MybatisPlusMetaObjectHandler implements MetaObjectHandler {
         String userId = requestAttributes.getRequest().getHeader("userId");
         if (userId == null || userId.equals("")) return;
         this.setFieldValByName("creatorId", Long.parseLong(userId), metaObject);
-        this.setFieldValByName("creatorId", Long.parseLong(userId), metaObject);
+        this.setFieldValByName("updaterId", Long.parseLong(userId), metaObject);
     }
 
     @Override
@@ -43,6 +43,6 @@ public class MybatisPlusMetaObjectHandler implements MetaObjectHandler {
         if (requestAttributes == null) return;
         String userId = requestAttributes.getRequest().getHeader("userId");
         if (userId == null || userId.equals("")) return;
-        this.setFieldValByName("creatorId", Long.parseLong(userId), metaObject);
+        this.setFieldValByName("updaterId", Long.parseLong(userId), metaObject);
     }
 }
