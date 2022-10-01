86a770f377d66ae5f5d1f23e49a9d465a4da1081
Moment <momincong@foxmail.com>
2022-10-01 18:32:36 +0800

    feat: add register by qq 新增qq注册

---
+++ genshin-map-commons/genshin-map-commons-web/src/main/java/site/yuanshen/common/web/config/RestTemplateConfig.java
@@ -0,0 +1,34 @@
+package site.yuanshen.common.web.config;
+
+import okhttp3.OkHttpClient;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.OkHttp3ClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.concurrent.TimeUnit;
+
+@Configuration
+public class RestTemplateConfig {
+
+    @ConditionalOnMissingBean(RestTemplate.class)
+    @Bean
+    public RestTemplate restTemplate(){
+        return new RestTemplate(getClientHttpRequestFactory());
+    }
+
+    /**
+     * 使用OkHttpClient作为底层客户端
+     */
+    private ClientHttpRequestFactory getClientHttpRequestFactory(){
+        OkHttpClient okHttpClient = new OkHttpClient.Builder()
+                .connectTimeout(5, TimeUnit.SECONDS)
+                .writeTimeout(5, TimeUnit.SECONDS)
+                .readTimeout(5, TimeUnit.SECONDS)
+                .build();
+        return new OkHttp3ClientHttpRequestFactory(okHttpClient);
+    }
+
+}
