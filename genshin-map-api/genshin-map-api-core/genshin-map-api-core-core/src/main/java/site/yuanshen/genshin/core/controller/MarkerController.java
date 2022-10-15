d1f9b27a4ef4a9377a5c1f939e2d63f7bad9c3f6
Alex Fang <deeno@qq.com>
2022-10-16 06:42:09 +0800

    refactor: split Controller url pattern for `punctuate`

--- genshin-map-api/genshin-map-api-core/genshin-map-api-core-core/src/main/java/site/yuanshen/genshin/core/controller/MarkerController.java
+++ genshin-map-api/genshin-map-api-core/genshin-map-api-core-core/src/main/java/site/yuanshen/genshin/core/controller/MarkerController.java
@@ -7,9 +7,14 @@ import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 import site.yuanshen.common.web.response.R;
 import site.yuanshen.common.web.response.RUtils;
-import site.yuanshen.data.dto.*;
+import site.yuanshen.data.dto.MarkerDto;
+import site.yuanshen.data.dto.MarkerExtraDto;
+import site.yuanshen.data.dto.MarkerSingleDto;
 import site.yuanshen.data.dto.helper.PageSearchDto;
-import site.yuanshen.data.vo.*;
+import site.yuanshen.data.vo.MarkerExtraVo;
+import site.yuanshen.data.vo.MarkerSearchVo;
+import site.yuanshen.data.vo.MarkerSingleVo;
+import site.yuanshen.data.vo.MarkerVo;
 import site.yuanshen.data.vo.helper.PageListVo;
 import site.yuanshen.data.vo.helper.PageSearchVo;
 import site.yuanshen.genshin.core.service.CacheService;
@@ -126,84 +131,5 @@ public class MarkerController {
     }
 
     //////////////END:点位自身的API//////////////
-
-    //////////////START:打点员的API//////////////
-
-    @Operation(summary = "分页查询所有打点信息", description = "分页查询所有打点信息")
-    @PostMapping("/punctuate/author/get/page")
-    public R<PageListVo<MarkerPunctuateVo>> listPunctuatePage(@RequestBody PageSearchVo pageSearchVo) {
-        return RUtils.create(
-                markerService.listPunctuatePage(new PageSearchDto(pageSearchVo))
-        );
-    }
-
-    @Operation(summary = "分页查询自己提交的未通过的打点信息（不包含额外字段）",
-            description = "分页查询自己提交的未通过的打点信息（不包含额外字段）（打点员的API）")
-    @PostMapping("/punctuate/author/get/page_single/{authorId}")
-    public R<PageListVo<MarkerSinglePunctuateVo>> listSelfSinglePunctuatePage(@RequestBody PageSearchVo pageSearchVo, @PathVariable("authorId") Long authorId) {
-        return RUtils.create(
-                markerService.listSelfSinglePunctuatePage(new PageSearchDto(pageSearchVo), authorId)
-        );
-    }
-
-    @Operation(summary = "分页查询自己提交的未通过的打点信息（只包含额外字段）",
-            description = "分页查询自己提交的未通过的打点信息（只包含额外字段） （打点员的API）")
-    @PostMapping("/punctuate/author/get/page_extra/{authorId}")
-    public R<PageListVo<MarkerExtraPunctuateVo>> listSelfExtraPunctuatePage(@RequestBody PageSearchVo pageSearchVo, @PathVariable("authorId") Long authorId) {
-        return RUtils.create(
-                markerService.listSelfExtraPunctuatePage(new PageSearchDto(pageSearchVo), authorId)
-        );
-    }
-
-    @Operation(summary = "提交暂存点位（不含额外字段）", description = "成功则返回打点提交ID")
-    @PutMapping("/punctuate/author/single")
-    public R<Long> addSinglePunctuate(@RequestBody MarkerSinglePunctuateVo markerSinglePunctuateVo) {
-        return RUtils.create(
-                markerService.addSinglePunctuate(new MarkerSinglePunctuateDto(markerSinglePunctuateVo))
-        );
-    }
-
-    @Operation(summary = "提交暂存点位额外字段", description = "在涉及的所有点位已经暂存后在使用该api")
-    @PutMapping("/punctuate/author/extra")
-    public R<Boolean> addExtraPunctuate(@RequestBody MarkerExtraPunctuateVo markerExtraPunctuateVo) {
-        return RUtils.create(
-                markerService.addExtraPunctuate(new MarkerExtraPunctuateDto(markerExtraPunctuateVo))
-        );
-    }
-
-    @Operation(summary = "将暂存点位提交审核", description = "将暂存点位提交审核")
-    @PutMapping("/punctuate/author/push/{authorId}")
-    public R<Boolean> pushPunctuate(@PathVariable("authorId") Long authorId) {
-        return RUtils.create(
-                markerService.pushPunctuate(authorId)
-        );
-    }
-
-    @Operation(summary = "修改自身未提交的暂存点位（不包括额外字段）", description = "根据点位ID修改点位")
-    @PostMapping("/punctuate/author/single")
-    public R<Boolean> updateSelfSinglePunctuate(@RequestBody MarkerSinglePunctuateVo singlePunctuateVo) {
-        return RUtils.create(
-                markerService.updateSelfSinglePunctuate(new MarkerSinglePunctuateDto(singlePunctuateVo))
-        );
-    }
-
-    @Operation(summary = "修改自身未提交的暂存点位的额外字段", description = "根据点位ID修改点位")
-    @PostMapping("/punctuate/author/extra")
-    public R<Boolean> updateSelfPunctuateExtra(@RequestBody MarkerExtraPunctuateVo extraPunctuateVo) {
-        return RUtils.create(
-                markerService.updateSelfPunctuateExtra(new MarkerExtraPunctuateDto(extraPunctuateVo))
-        );
-    }
-
-    @Operation(summary = "删除自己未通过的提交点位", description = "根据提交ID列表来删除提交点位，会对打点员ID进行校验")
-    @DeleteMapping("/punctuate/author/{authorId}/{punctuateId}")
-    public R<Boolean> deleteSelfPunctuate(@PathVariable("punctuateId") Long punctuateId, @PathVariable("authorId") Long authorId) {
-        return RUtils.create(
-                markerService.deleteSelfPunctuate(punctuateId, authorId)
-        );
-    }
-
-    //////////////END:打点员的API//////////////
-
 
 }
