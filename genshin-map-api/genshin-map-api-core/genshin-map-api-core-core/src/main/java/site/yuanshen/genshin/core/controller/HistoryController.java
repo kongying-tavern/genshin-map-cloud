4cc9decaaf3523f778d1c3185687340bfd239abc
Moment <momincong@foxmail.com>
2023-08-06 17:32:33 +0800

    清除无用API

--- genshin-map-api/genshin-map-api-core/genshin-map-api-core-core/src/main/java/site/yuanshen/genshin/core/controller/HistoryController.java
+++ genshin-map-api/genshin-map-api-core/genshin-map-api-core-core/src/main/java/site/yuanshen/genshin/core/controller/HistoryController.java
@@ -1,24 +1,16 @@
 package site.yuanshen.genshin.core.controller;
 
 
-import com.baomidou.mybatisplus.core.toolkit.Wrappers;
-import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
-import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 import site.yuanshen.common.web.response.R;
 import site.yuanshen.common.web.response.RUtils;
 import site.yuanshen.data.dto.HistorySearchDto;
-import site.yuanshen.data.dto.ItemSearchDto;
-import site.yuanshen.data.entity.History;
 import site.yuanshen.data.vo.HistorySearchVo;
 import site.yuanshen.data.vo.HistoryVo;
-import site.yuanshen.data.vo.ItemSearchVo;
-import site.yuanshen.data.vo.ItemVo;
 import site.yuanshen.data.vo.helper.PageListVo;
-import site.yuanshen.genshin.core.manager.HistoryManager;
 import site.yuanshen.genshin.core.service.HistoryService;
 import site.yuanshen.genshin.core.service.UserAppenderService;
 
@@ -33,20 +25,6 @@ import site.yuanshen.genshin.core.service.UserAppenderService;
 @Tag(name = "history", description = "历史记录API")
 public class HistoryController {
     private final HistoryService historyService;
-    private final HistoryManager historyManager;
-
-
-    /**
-     * 历史记录分页
-     *
-     * @return R
-     */
-
-//    @PostMapping("/get/page")
-//    public R<Page<History>> listHistory(Page<History> page, Integer type, Long id) {
-//        return RUtils.create(historyService.page(page, Wrappers.<History>lambdaQuery().eq(History::getType, type).eq(History::getTId, id)));
-//    }
-
 
     @Operation(summary = "历史记录分页", description = "历史记录分页")
     @PostMapping("/get/list")
@@ -57,17 +35,6 @@ public class HistoryController {
         UserAppenderService.appendUser(result, result.getData().getRecord(), true, HistoryVo::getCreatorId);
         UserAppenderService.appendUser(result, result.getData().getRecord(), true, HistoryVo::getUpdaterId);
         return result;
-    }
-
-    /**
-     * 回滚记录
-     *
-     * @return R
-     */
-    @Operation(summary = "回滚记录", description = "回滚记录")
-    @PostMapping("/rollback")
-    public R<Boolean> rollback(Long id) {
-        return RUtils.create(historyManager.rollback(id));
     }
 
 }
