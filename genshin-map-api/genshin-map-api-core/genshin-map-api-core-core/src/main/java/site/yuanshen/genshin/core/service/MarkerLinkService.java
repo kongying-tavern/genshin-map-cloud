1df3e5d2f5ce8fca6f8b7ad06d18785f0b4a63a4
Alex Fang <deeno@qq.com>
2023-10-27 16:19:11 +0800

    refactor: move util functions for marker linkage to a separate helper

--- genshin-map-api/genshin-map-api-core/genshin-map-api-core-core/src/main/java/site/yuanshen/genshin/core/service/MarkerLinkService.java
+++ genshin-map-api/genshin-map-api-core/genshin-map-api-core-core/src/main/java/site/yuanshen/genshin/core/service/MarkerLinkService.java
@@ -1,10 +1,8 @@
 package site.yuanshen.genshin.core.service;
 
 import cn.hutool.core.collection.CollUtil;
-import cn.hutool.core.util.ByteUtil;
 import cn.hutool.core.util.IdUtil;
 import cn.hutool.core.util.ObjectUtil;
-import cn.hutool.crypto.SecureUtil;
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import lombok.RequiredArgsConstructor;
 import org.springframework.cache.annotation.Cacheable;
@@ -14,12 +12,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import site.yuanshen.common.core.exception.GenshinApiException;
 import site.yuanshen.common.core.utils.BeanUtils;
 import site.yuanshen.data.entity.MarkerLinkage;
+import site.yuanshen.data.helper.MarkerLinkageDataHelper;
 import site.yuanshen.data.vo.MarkerLinkageSearchVo;
 import site.yuanshen.data.vo.MarkerLinkageVo;
 import site.yuanshen.genshin.core.dao.MarkerLinkageDao;
 import site.yuanshen.genshin.core.service.mbp.MarkerLinkageMBPService;
 
-import java.nio.ByteOrder;
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -45,7 +43,7 @@ public class MarkerLinkService {
 
         List<MarkerLinkageVo> linkageList = markerLinkageMBPService.list(Wrappers.<MarkerLinkage>lambdaQuery().in(MarkerLinkage::getGroupId, groupIds)).stream()
             .map(markerLinkage -> BeanUtils.copy(markerLinkage, MarkerLinkageVo.class)).collect(Collectors.toList());
-        reverseLinkageIds(linkageList);
+        MarkerLinkageDataHelper.reverseLinkageIds(linkageList);
         Map<String, List<MarkerLinkageVo>> linkageMap = linkageList.parallelStream().collect(Collectors.groupingBy(MarkerLinkageVo::getGroupId));
 
         return linkageMap;
@@ -61,15 +59,15 @@ public class MarkerLinkService {
         final String groupId = IdUtil.fastSimpleUUID();
 
         // 获取现有的列表
-        final List<Long> idList = this.getLinkIdList(linkageVos);
+        final List<Long> idList = MarkerLinkageDataHelper.getLinkIdList(linkageVos);
         final List<MarkerLinkage> linkageExistsList = markerLinkageDao.getRelatedLinkageList(idList, true)
             .parallelStream()
             .map(v -> BeanUtils.copy(v, MarkerLinkage.class))
             .collect(Collectors.toList());
 
         // 生成更新数据的数据列表
-        Map<String, MarkerLinkage> linkageMap = this.getLinkSearchMap(linkageExistsList);
-        linkageMap = this.patchLinkSearchMap(linkageMap, linkageVos, groupId);
+        Map<String, MarkerLinkage> linkageMap = MarkerLinkageDataHelper.getLinkSearchMap(linkageExistsList);
+        linkageMap = MarkerLinkageDataHelper.patchLinkSearchMap(linkageMap, linkageVos, groupId);
         List<MarkerLinkage> linkageList = new ArrayList<>(linkageMap.values());
 
         // 更新数据
@@ -77,20 +75,7 @@ public class MarkerLinkService {
         return linkSuccess ? groupId : "";
     }
 
-    private void reverseLinkageIds(List<MarkerLinkageVo> linkageVos) {
-        for(MarkerLinkageVo linkageVo : linkageVos) {
-            final Long fromId = ObjectUtil.defaultIfNull(linkageVo.getFromId(), 0L);
-            final Long toId = ObjectUtil.defaultIfNull(linkageVo.getToId(), 0L);
-            final Boolean linkReverse = ObjectUtil.defaultIfNull(linkageVo.getLinkReverse(), false);
-
-            if(linkReverse) {
-                linkageVo.setFromId(toId);
-                linkageVo.setToId(fromId);
-                linkageVo.setLinkReverse(false);
-            }
-        }
-    }
-
+    // -------------------------------------------
     private void checkLinkList(List<MarkerLinkageVo> linkageVos) {
         for(MarkerLinkageVo linkageVo : linkageVos) {
             final Long fromId = linkageVo.getFromId();
@@ -104,74 +89,5 @@ public class MarkerLinkService {
         if(CollUtil.isEmpty(linkageVos)) {
             throw new GenshinApiException("关联数据不可为空");
         }
-    }
-
-    private List<Long> getLinkIdList(List<MarkerLinkageVo> linkageVos) {
-        // 生成用到的 ID
-        final Set<Long> idSet = new HashSet<>();
-        for(MarkerLinkageVo linkage : linkageVos) {
-            idSet.add(linkage.getFromId());
-            idSet.add(linkage.getToId());
-        }
-        return idSet.parallelStream().filter(Objects::nonNull).collect(Collectors.toList());
-    }
-
-    private Map<String, MarkerLinkage> getLinkSearchMap(List<MarkerLinkage> linkageList) {
-        final Map<String, MarkerLinkage> searchMap = new HashMap<>();
-        for(MarkerLinkage linkageEntity : linkageList) {
-            final String idHash = getIdHash(Arrays.asList(linkageEntity.getFromId(), linkageEntity.getToId()));
-            searchMap.put(idHash, linkageEntity);
-        }
-        return searchMap;
-    }
-
-    private Map<String, MarkerLinkage> patchLinkSearchMap(Map<String, MarkerLinkage> linkageMap, List<MarkerLinkageVo> linkageVos, String groupId) {
-        // 先设置所有的关联为删除，后续对新增关联开启，以便复用现有关联
-        linkageMap.replaceAll((hash, v) -> {
-            v.setDelFlag(true);
-            return v;
-        });
-
-        for(MarkerLinkageVo linkageVo : linkageVos) {
-            Long fromId = linkageVo.getFromId();
-            Long toId = linkageVo.getToId();
-            if(fromId == null || toId == null) {
-                continue;
-            }
-            boolean dirReverse = false;
-            if(fromId.compareTo(toId) > 0) {
-                fromId = fromId ^ toId;
-                toId = fromId ^ toId;
-                fromId = fromId ^ toId;
-                dirReverse = true;
-            }
-            final String idHash = this.getIdHash(Arrays.asList(linkageVo.getFromId(), linkageVo.getToId()));
-            final MarkerLinkage linkageItem = linkageMap.getOrDefault(idHash, new MarkerLinkage());
-            linkageItem.setGroupId(groupId);
-            linkageItem.setFromId(fromId);
-            linkageItem.setToId(toId);
-            linkageItem.setLinkAction(StrUtil.blankToDefault(linkageVo.getLinkAction(), ""));
-            linkageItem.setLinkReverse(dirReverse);
-            linkageItem.setPath(CollUtil.defaultIfEmpty(linkageVo.getPath(), List.of()));
-            linkageItem.setDelFlag(false);
-
-            linkageMap.put(idHash, linkageItem);
-        }
-
-        return linkageMap;
-    }
-
-    private String getIdHash(List<Long> idList) {
-        idList = idList.stream().map(id -> ObjectUtil.defaultIfNull(id, 0L)).sorted().collect(Collectors.toList());
-        int byteSize = Long.BYTES * idList.size();
-        byte[] bytes = new byte[byteSize];
-
-        for(int i = 0; i < idList.size(); i++) {
-            Long id = idList.get(i);
-            byte[] idBytes = ByteUtil.longToBytes(id, ByteOrder.BIG_ENDIAN);
-            System.arraycopy(idBytes, 0, bytes, i * Long.BYTES, Long.BYTES);
-        }
-        final String idHash = SecureUtil.md5(Arrays.toString(bytes));
-        return idHash;
     }
 }
