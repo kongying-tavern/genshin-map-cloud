03a6bb31b9a7e1cc876d2d9fcb1ebe44919485cc
Alex Fang <deeno@qq.com>
2023-12-05 02:23:44 +0800

    feat: 添加历史记录排序

--- genshin-map-api/genshin-map-api-core/genshin-map-api-core-core/src/main/java/site/yuanshen/genshin/core/service/HistoryService.java
+++ genshin-map-api/genshin-map-api-core/genshin-map-api-core-core/src/main/java/site/yuanshen/genshin/core/service/HistoryService.java
@@ -1,11 +1,15 @@
 package site.yuanshen.genshin.core.service;
 
 import cn.hutool.core.util.ObjectUtil;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import site.yuanshen.common.core.utils.PgsqlUtils;
+import site.yuanshen.data.base.BaseEntity;
 import site.yuanshen.data.dto.HistoryDto;
 import site.yuanshen.data.dto.HistorySearchDto;
 import site.yuanshen.data.entity.History;
@@ -15,6 +19,7 @@ import site.yuanshen.data.vo.helper.PageListVo;
 
 import java.util.Comparator;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 @Service
@@ -24,15 +29,21 @@ public class HistoryService extends ServiceImpl<HistoryMapper, History> {
     private final HistoryMapper historyMapper;
 
     public PageListVo<HistoryVo> listPage(HistorySearchDto historySearchDto) {
-        Page<History> historyPage = historyMapper.selectPage(historySearchDto.getPageEntity(),
-                Wrappers.<History>lambdaQuery()
-                        .eq(ObjectUtil.isNotNull(historySearchDto.getType()),History::getType, historySearchDto.getType())
-                        .in(!historySearchDto.getId().isEmpty(),History::getTId, historySearchDto.getId()));
+        QueryWrapper<History> wrapper = Wrappers.<History>query();
+        // 处理排序
+        final List<PgsqlUtils.Sort<History>> sortList = PgsqlUtils.toSort(historySearchDto.getSort(), History.class, Set.of("updateTime"));
+        wrapper = PgsqlUtils.sortWrapper(wrapper, sortList);
+
+        LambdaQueryWrapper<History> queryWrapper = wrapper.lambda()
+                .eq(ObjectUtil.isNotNull(historySearchDto.getType()), History::getType, historySearchDto.getType())
+                .in(!historySearchDto.getId().isEmpty(), History::getTId, historySearchDto.getId());
+
+        Page<History> historyPage = historyMapper.selectPage(historySearchDto.getPageEntity(), queryWrapper);
 
         List<HistoryVo> result = historyPage.getRecords().stream()
                 .map(HistoryDto::new)
                 .map(HistoryDto::getVo)
-                .sorted(Comparator.comparing(HistoryVo::getUpdateTime)).collect(Collectors.toList());
+                .collect(Collectors.toList());
         return new PageListVo<HistoryVo>()
                 .setRecord(result)
                 .setTotal(historyPage.getTotal())
--- genshin-map-commons/genshin-map-commons-core/src/main/java/site/yuanshen/common/core/utils/PgsqlUtils.java
+++ genshin-map-commons/genshin-map-commons-core/src/main/java/site/yuanshen/common/core/utils/PgsqlUtils.java
@@ -1,9 +1,19 @@
 package site.yuanshen.common.core.utils;
 
+import cn.hutool.core.collection.CollUtil;
+import cn.hutool.core.text.NamingCase;
+import cn.hutool.core.util.StrUtil;
 import com.alibaba.fastjson2.JSON;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-import java.util.Collections;
-import java.util.List;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.util.*;
+import java.util.function.Function;
 
 /**
  * 为了应付pg里的奇妙查询格式
@@ -19,5 +29,119 @@ public class PgsqlUtils {
         String s = JSON.toJSONString(strList);
         s = s.replace("\"", "");
         return "'{" + s.substring(1,s.length()-1) + "}'";
+    }
+
+    public enum Order {
+        ASC,
+        DESC
+    }
+
+    @NoArgsConstructor
+    @Data
+    public static class Sort<E> {
+        private String field = "";
+        private String prop = "";
+        private Function<E, ?> osGetter;
+        private Order order;
+    }
+
+    public static <E> List<Sort<E>> toSort(List<String> sorts, Class<E> clazz, Set<String> allowProps) {
+        if (CollUtil.isEmpty(sorts)) {
+            return List.of();
+        }
+
+        // 提取字段名和初始排序映射
+        final Map<String, Order> sortOrderMap = new HashMap<>();
+        final List<String> sortPropList = new ArrayList<>();
+        final Set<String> sortPropSet = new HashSet<>();
+        for (String sortChunk : sorts) {
+            if(StrUtil.isBlank(sortChunk)) {
+                continue;
+            }
+
+            String sortProp = sortChunk.substring(0, sortChunk.length() - 1);
+            String sortOrder = sortChunk.substring(sortChunk.length() - 1);
+            if (
+                    StrUtil.equalsAnyIgnoreCase(sortOrder, "+", "-") &&
+                    StrUtil.isNotBlank(sortProp)
+            ) {
+                if (CollUtil.isEmpty(allowProps) || !allowProps.contains(sortProp)) {
+                    continue;
+                }
+
+                final Order sortOrderEnum = "+".equals(sortOrder) ? Order.ASC : Order.DESC;
+                sortPropSet.add(sortProp);
+                sortPropList.add(sortProp);
+                sortOrderMap.put(sortProp, sortOrderEnum);
+            }
+        }
+
+        // 提取字段映射
+        final List<Field> fields = ClassUtils.getFields(clazz);
+        final Map<String, Field> fieldMap = new HashMap<>();
+        for (Field field : fields) {
+            final String propName = field.getName();
+            if (sortPropSet.contains(propName)) {
+                fieldMap.put(propName, field);
+            }
+        }
+
+        // 构造排序
+        final List<Sort<E>> sortList = new ArrayList<>();
+        for (String propName : sortPropList) {
+            if (!sortPropSet.contains(propName)) {
+                continue;
+            }
+
+            final Order sortOrder = sortOrderMap.get(propName);
+            final Field sortField = fieldMap.get(propName);
+            if (sortOrder == null || sortField == null) {
+                continue;
+            }
+
+            String sortFieldName = "";
+            final TableField annotation = sortField.getDeclaredAnnotation(TableField.class);
+            if(annotation != null) {
+                sortFieldName = StrUtil.blankToDefault(annotation.value(), "");
+            }
+            if(StrUtil.isBlank(sortFieldName)) {
+                sortFieldName = NamingCase.toSymbolCase(propName, '_');
+            }
+
+            Function<E, ?> sortGetterFunction = null;
+            try {
+                final String sortGetterName = "get" + NamingCase.toPascalCase(propName);
+                final Method sortGetter = clazz.getDeclaredMethod(sortGetterName, new Class[]{});
+                sortGetterFunction = (i) -> {
+                    try {
+                        return sortGetter.invoke(i);
+                    } catch(Exception ex) {
+                        return null;
+                    }
+                };
+            } catch (Exception e) {
+                // nothing
+            }
+
+            Sort<E> sort = (new Sort<>());
+            sort.setField(sortFieldName);
+            sort.setProp(propName);
+            sort.setOsGetter(sortGetterFunction);
+            sort.setOrder(sortOrder);
+            sortList.add(sort);
+        }
+
+        return sortList;
+    }
+
+    public static <E> QueryWrapper<E> sortWrapper(QueryWrapper<E> wrapper, List<Sort<E>> sortList) {
+        for (Sort<E> sortItem : sortList) {
+            wrapper = wrapper.orderBy(
+                    StrUtil.isNotEmpty(sortItem.getField()),
+                    Order.ASC.equals(sortItem.getOrder()),
+                    sortItem.getField()
+            );
+        }
+        return wrapper;
     }
 }
