ec90c7b29d5c13b9b557f5918dbdd3958c1c5696
Alex Fang <deeno@qq.com>
2024-01-05 19:48:19 +0800

    chore: remove unused files

--- genshin-map-data/genshin-map-data-core/genshin-map-data-core-model/src/main/java/site/yuanshen/data/enums/marker/tweak/TweakPropertyEnum.java
+++ genshin-map-data/genshin-map-data-core/genshin-map-data-core-model/src/main/java/site/yuanshen/data/enums/marker/tweak/TweakPropertyEnum.java
@@ -16,40 +16,39 @@ import java.util.function.Function;
 @Getter
 public enum TweakPropertyEnum {
     TITLE(
-            "title", String.class,
+            "title",
             MarkerDto::getMarkerTitle,
             (marker, data) -> marker.setMarkerTitle((String) data)
     ),
     CONTENT(
-            "content", String.class,
+            "content",
             MarkerDto::getContent,
             (marker, data) -> marker.setContent((String) data)
     ),
     REFRESH_TIME(
-            "refreshTime", Long.class,
+            "refreshTime",
             MarkerDto::getRefreshTime,
             (marker, data) -> marker.setRefreshTime(NumberUtil.parseLong(data.toString()))
     ),
     HIDDEN_FLAG(
-            "hiddenFlag", Integer.class,
+            "hiddenFlag",
             MarkerDto::getHiddenFlag,
             (marker, data) -> marker.setHiddenFlag(NumberUtil.parseInt(data.toString()))
     ),
     EXTRA(
-            "extra", Map.class,
+            "extra",
             MarkerDto::getExtra,
             (marker, data) -> marker.setExtra((Map<String, Object>) data)
     ),
     ITEM_LIST(
-            "itemList", List.class,
+            "itemList",
             MarkerDto::getItemList,
-            (markerDto, data) -> markerDto.setItemList((List<MarkerItemLinkVo>) data)
+            (marker, data) -> marker.setItemList((List<MarkerItemLinkVo>) data)
     );
 
     @JsonValue
     private final String name;
 
-    private final Class<?> propClass;
     private final Function<MarkerDto, ?> getter;
     private final BiConsumer<MarkerDto, ?> setter;
 }
