9c2921a5d31cd240cee2ac25c5b28801bf399c27
Alex Fang <deeno@qq.com>
2024-01-05 21:16:45 +0800

    chore: 修改方法寻址逻辑

--- genshin-map-data/genshin-map-data-core/genshin-map-data-core-model/src/main/java/site/yuanshen/data/enums/marker/tweak/TweakHandlerEnum.java
+++ genshin-map-data/genshin-map-data-core/genshin-map-data-core-model/src/main/java/site/yuanshen/data/enums/marker/tweak/TweakHandlerEnum.java
@@ -10,107 +10,131 @@ import java.util.Objects;
 public enum TweakHandlerEnum {
     // TITLE
     TITLE$$UPDATE(
+            "title", "update",
             TweakPropertyEnum.TITLE,
             TweakTypeEnum.UPDATE
     ),
     TITLE$$REPLACE(
+            "title", "replace",
             TweakPropertyEnum.TITLE,
             TweakTypeEnum.REPLACE
     ),
     TITLE$$REPLACE_REGEX(
+            "title", "replaceRegex",
             TweakPropertyEnum.TITLE,
             TweakTypeEnum.REPLACE_REGEX
     ),
     TITLE$$PREPEND(
+            "title", "prepend",
             TweakPropertyEnum.TITLE,
             TweakTypeEnum.PREPEND
     ),
     TITLE$$APPEND(
+            "title", "append",
             TweakPropertyEnum.TITLE,
             TweakTypeEnum.APPEND
     ),
     TITLE$$TRIM_LEFT(
+            "title", "trimLeft",
             TweakPropertyEnum.TITLE,
             TweakTypeEnum.TRIM_LEFT
     ),
     TITLE$$TRIM_RIGHT(
+            "title", "trimRight",
             TweakPropertyEnum.TITLE,
             TweakTypeEnum.TRIM_RIGHT
     ),
     TITLE$$REMOVE_LEFT(
+            "title", "removeLeft",
             TweakPropertyEnum.TITLE,
             TweakTypeEnum.REMOVE_LEFT
     ),
     TITLE$$REMOVE_RIGHT(
+            "title", "removeRight",
             TweakPropertyEnum.TITLE,
             TweakTypeEnum.REMOVE_RIGHT
     ),
     // CONTENT
     CONTENT$$UPDATE(
+            "content", "update",
             TweakPropertyEnum.CONTENT,
             TweakTypeEnum.UPDATE
     ),
     CONTENT$$REPLACE(
+            "content", "replace",
             TweakPropertyEnum.CONTENT,
             TweakTypeEnum.REPLACE
     ),
     CONTENT$$REPLACE_REGEX(
+            "content", "replaceRegex",
             TweakPropertyEnum.CONTENT,
             TweakTypeEnum.REPLACE_REGEX
     ),
     CONTENT$$PREPEND(
+            "content", "prepend",
             TweakPropertyEnum.CONTENT,
             TweakTypeEnum.PREPEND
     ),
     CONTENT$$APPEND(
+            "content", "append",
             TweakPropertyEnum.CONTENT,
             TweakTypeEnum.APPEND
     ),
     CONTENT$$TRIM_LEFT(
+            "content", "trimLeft",
             TweakPropertyEnum.CONTENT,
             TweakTypeEnum.TRIM_LEFT
     ),
     CONTENT$$TRIM_RIGHT(
+            "content", "trimRight",
             TweakPropertyEnum.CONTENT,
             TweakTypeEnum.TRIM_RIGHT
     ),
     CONTENT$$REMOVE_LEFT(
+            "content", "removeLeft",
             TweakPropertyEnum.CONTENT,
             TweakTypeEnum.REMOVE_LEFT
     ),
     CONTENT$$REMOVE_RIGHT(
+            "content", "removeRight",
             TweakPropertyEnum.CONTENT,
             TweakTypeEnum.REMOVE_RIGHT
     ),
     // REFRESH_TIME
     REFRESH_TIME$$UPDATE(
+            "refreshTime", "update",
             TweakPropertyEnum.REFRESH_TIME,
             TweakTypeEnum.UPDATE
     ),
     // HIDDEN_FLAG
     HIDDEN_FLAG$$UPDATE(
+            "hiddenFlag", "update",
             TweakPropertyEnum.HIDDEN_FLAG,
             TweakTypeEnum.UPDATE
     ),
     // EXTRA
     EXTRA$$MERGE(
+            "extra", "merge",
             TweakPropertyEnum.EXTRA,
             TweakTypeEnum.MERGE
     ),
     // ITEM_LIST
     ITEM_LIST$$UPDATE(
+            "itemList", "update",
             TweakPropertyEnum.ITEM_LIST,
             TweakTypeEnum.UPDATE
     );
 
+    private final String propName;
+    private final String typeName;
     private final TweakPropertyEnum prop;
     private final TweakTypeEnum type;
 
-    public static TweakHandlerEnum find(TweakPropertyEnum prop, TweakTypeEnum type) {
+    public static TweakHandlerEnum find(String propName, String typeName) {
         for(TweakHandlerEnum handler : TweakHandlerEnum.values()) {
-            final TweakPropertyEnum handlerProp = handler.prop;
-            TweakTypeEnum handlerType = handler.type;
-            if(Objects.equals(handlerProp, prop) && Objects.equals(handlerType, type)) {
+            final String handlerProp = handler.propName;
+            final String handlerType = handler.typeName;
+            if(Objects.equals(handlerProp, propName) && Objects.equals(handlerType, typeName)) {
                 return handler;
             }
         }
--- genshin-map-data/genshin-map-data-core/genshin-map-data-core-model/src/main/java/site/yuanshen/data/vo/adapter/marker/tweak/TweakConfigVo.java
+++ genshin-map-data/genshin-map-data-core/genshin-map-data-core-model/src/main/java/site/yuanshen/data/vo/adapter/marker/tweak/TweakConfigVo.java
@@ -9,10 +9,10 @@ import site.yuanshen.data.enums.marker.tweak.TweakTypeEnum;
 @Schema(title = "点位调整配置项前端封装")
 public class TweakConfigVo {
     @Schema(title = "需调整的点位属性")
-    private TweakPropertyEnum prop;
+    private String prop;
 
     @Schema(title = "调整方法类型")
-    private TweakTypeEnum type;
+    private String type;
 
     @Schema(title = "调整配置数据", description = "此项根据调整方法类型使用不同的数据字段")
     private TweakConfigMetaVo meta = new TweakConfigMetaVo();
