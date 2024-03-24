66f66f96e0f9ff73bbf2a4d6cfe648b66d83606c
Alex Fang <deeno@qq.com>
2024-03-25 06:48:46 +0800

    fix: 修复点位缓存存储

--- genshin-map-api/genshin-map-api-core/genshin-map-api-core-core/src/main/java/site/yuanshen/genshin/core/service/MarkerDocService.java
+++ genshin-map-api/genshin-map-api-core/genshin-map-api-core-core/src/main/java/site/yuanshen/genshin/core/service/MarkerDocService.java
@@ -9,9 +9,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.util.DigestUtils;
 import site.yuanshen.genshin.core.dao.MarkerDao;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 /**
@@ -31,16 +29,15 @@ public class MarkerDocService {
      *
      * @return 分页字节数组的md5
      */
-
     @Cacheable(value = "listMarkerBz2MD5", cacheManager = "neverRefreshCacheManager")
-    public List<String> listMarkerBz2MD5() {
-        return new ArrayList<>();
+    public Map<String, String> listAllMarkerBz2MD5() {
+        return new LinkedHashMap<>();
     }
 
     @CachePut(value = "listMarkerBz2MD5", cacheManager = "neverRefreshCacheManager")
     public Map<String, String> refreshMarkerBz2MD5() {
         long startTime = System.currentTimeMillis();
-        Map<String, String> result = new HashMap<>();
+        Map<String, String> result = new LinkedHashMap<>();
         Map<String, byte[]> bz2Map = markerDao.refreshPageMarkerByBz2();
         for(Map.Entry<String, byte[]> bz2Entry : bz2Map.entrySet()) {
             result.put(bz2Entry.getKey(), DigestUtils.md5DigestAsHex(bz2Entry.getValue()));
