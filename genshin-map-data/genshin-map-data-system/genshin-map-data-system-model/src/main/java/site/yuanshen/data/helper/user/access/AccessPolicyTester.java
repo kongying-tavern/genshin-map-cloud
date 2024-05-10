93999da085a4cdddc3620511448cd641005794f9
Alex Fang <deeno@qq.com>
2024-05-10 17:10:32 +0800

    feat: 添加权限拦截策略：与最后一次登录IP相同
    Co-authored-by: dango0911 <1457087747@qq.com>

--- genshin-map-data/genshin-map-data-system/genshin-map-data-system-model/src/main/java/site/yuanshen/data/helper/user/access/AccessPolicyTester.java
+++ genshin-map-data/genshin-map-data-system/genshin-map-data-system-model/src/main/java/site/yuanshen/data/helper/user/access/AccessPolicyTester.java
@@ -2,11 +2,22 @@ package site.yuanshen.data.helper.user.access;
 
 import site.yuanshen.data.dto.SysUserDeviceDto;
 
+import java.util.Comparator;
 import java.util.List;
+import java.util.Objects;
 
 public class AccessPolicyTester {
     public static boolean testIpWithSameLastIp(List<SysUserDeviceDto> deviceList, SysUserDeviceDto currentDevice) {
-        return true;
+        if(currentDevice == null)
+            return false;
+        SysUserDeviceDto lastLoginDevice = deviceList.stream()
+                .filter(v -> v != null && v.getLastLoginTime() != null)
+                .sorted(Comparator.comparing(SysUserDeviceDto::getLastLoginTime).reversed())
+                .findFirst()
+                .orElse(null);
+        if(lastLoginDevice == null)
+            return true;
+        return Objects.equals(lastLoginDevice.getIpv4(), currentDevice.getIpv4());
     }
 
     public static boolean testIpWithPassAllowIp(List<SysUserDeviceDto> deviceList, SysUserDeviceDto currentDevice) {
