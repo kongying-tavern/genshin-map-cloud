5dcdd16a2c17714e1668d17caec695034cfd88c7
Alex Fang <deeno@qq.com>
2024-06-05 16:10:20 +0800

    chore: 添加creatorId/createTime字段

--- genshin-map-data/genshin-map-data-system/genshin-map-data-system-model/src/main/java/site/yuanshen/data/vo/SysUserDeviceVo.java
+++ genshin-map-data/genshin-map-data-system/genshin-map-data-system-model/src/main/java/site/yuanshen/data/vo/SysUserDeviceVo.java
@@ -36,6 +36,19 @@ public class SysUserDeviceVo {
     private Long id;
 
     /**
+     * 创建人
+     */
+    @Schema(title = "创建人")
+    private Long creatorId;
+
+    /**
+     * 创建时间
+     */
+    @Schema(title = "创建时间")
+    @JsonFormat(shape = JsonFormat.Shape.NUMBER_INT)
+    private Timestamp createTime;
+
+    /**
      * 更新人
      */
     @Schema(title = "更新人")
