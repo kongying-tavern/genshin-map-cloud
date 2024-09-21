6e269768caf432d9ce8207ec8e4ceca7840168db
Alex Fang <deeno@qq.com>
2024-09-22 02:29:55 +0800

    feat: 添加有向关联、通道关联

--- genshin-map-data/genshin-map-data-core/genshin-map-data-core-model/src/main/java/site/yuanshen/data/helper/marker/linkage/MarkerLinkageGraphAccumulator.java
+++ genshin-map-data/genshin-map-data-core/genshin-map-data-core-model/src/main/java/site/yuanshen/data/helper/marker/linkage/MarkerLinkageGraphAccumulator.java
@@ -8,62 +8,34 @@ import java.util.List;
 
 public final class MarkerLinkageGraphAccumulator {
     public static void withTrigger(List<AccumulatorCache> caches, MarkerLinkageVo linkageVo) {
-        final LinkRefDto linkRef = MarkerLinkageDataHelper.getLinkRef(linkageVo);
-        final AccumulatorCache ac = new AccumulatorCache();
-        ac.addLinkage(linkRef);
-        ac.addPath(linkageVo);
-        caches.add(ac);
+        accumulateOneToOneTrigger(caches, linkageVo);
     }
 
     public static void withTriggerAll(List<AccumulatorCache> caches, MarkerLinkageVo linkageVo) {
-        final LinkRefDto linkRef = MarkerLinkageDataHelper.getLinkRef(linkageVo);
-        AccumulatorCache ac = null;
-
-        // Try to find existing matched accumulator cache
-        for (AccumulatorCache cache : caches) {
-            final Long toId = linkageVo.getToId();
-            if (cache.inLinkage(null, toId, false, true)) {
-                ac = cache;
-                break;
-            }
-        }
-
-        // No accumulator cache matched, add a new accumulator cache
-        if (ac == null) {
-            ac = new AccumulatorCache();
-            caches.add(ac);
-        }
-
-        // Add data
-        ac.addLinkage(linkRef);
-        ac.addPath(linkageVo);
+        accumulateAnyToAnyTrigger(caches, linkageVo);
     }
 
     public static void withTriggerAny(List<AccumulatorCache> caches, MarkerLinkageVo linkageVo) {
-        final LinkRefDto linkRef = MarkerLinkageDataHelper.getLinkRef(linkageVo);
-        AccumulatorCache ac = null;
-
-        // Try to find existing matched accumulator cache
-        for (AccumulatorCache cache : caches) {
-            final Long toId = linkageVo.getToId();
-            if (cache.inLinkage(null, toId, false, true)) {
-                ac = cache;
-                break;
-            }
-        }
-
-        // No accumulator cache matched, add a new accumulator cache
-        if (ac == null) {
-            ac = new AccumulatorCache();
-            caches.add(ac);
-        }
-
-        // Add data
-        ac.addLinkage(linkRef);
-        ac.addPath(linkageVo);
+        accumulateAnyToAnyTrigger(caches, linkageVo);
     }
 
     public static void withRelated(List<AccumulatorCache> caches, MarkerLinkageVo linkageVo) {
+        accumulateGroup(caches, linkageVo);
+    }
+
+    public static void withDirected(List<AccumulatorCache> caches, MarkerLinkageVo linkageVo) {
+        accumulateGroup(caches, linkageVo);
+    }
+
+    public static void withPathUniDir(List<AccumulatorCache> caches, MarkerLinkageVo linkageVo) {
+        accumulateGroup(caches, linkageVo);
+    }
+
+    public static void withPathBiDir(List<AccumulatorCache> caches, MarkerLinkageVo linkageVo) {
+        accumulateGroup(caches, linkageVo);
+    }
+
+    public static void withEquivalent(List<AccumulatorCache> caches, MarkerLinkageVo linkageVo) {
         final LinkRefDto linkRef = MarkerLinkageDataHelper.getLinkRef(linkageVo);
         AccumulatorCache ac = null;
 
@@ -86,7 +58,44 @@ public final class MarkerLinkageGraphAccumulator {
         ac.addPath(linkageVo);
     }
 
-    public static void withEquivalent(List<AccumulatorCache> caches, MarkerLinkageVo linkageVo) {
+    /**
+     * --------------------------------------------------
+     * Helper Functions
+     * --------------------------------------------------
+     */
+    private static void accumulateOneToOneTrigger(List<AccumulatorCache> caches, MarkerLinkageVo linkageVo) {
+        final LinkRefDto linkRef = MarkerLinkageDataHelper.getLinkRef(linkageVo);
+        final AccumulatorCache ac = new AccumulatorCache();
+        ac.addLinkage(linkRef);
+        ac.addPath(linkageVo);
+        caches.add(ac);
+    }
+
+    private static void accumulateAnyToAnyTrigger(List<AccumulatorCache> caches, MarkerLinkageVo linkageVo) {
+        final LinkRefDto linkRef = MarkerLinkageDataHelper.getLinkRef(linkageVo);
+        AccumulatorCache ac = null;
+
+        // Try to find existing matched accumulator cache
+        for (AccumulatorCache cache : caches) {
+            final Long toId = linkageVo.getToId();
+            if (cache.inLinkage(null, toId, false, true)) {
+                ac = cache;
+                break;
+            }
+        }
+
+        // No accumulator cache matched, add a new accumulator cache
+        if (ac == null) {
+            ac = new AccumulatorCache();
+            caches.add(ac);
+        }
+
+        // Add data
+        ac.addLinkage(linkRef);
+        ac.addPath(linkageVo);
+    }
+
+    private static void accumulateGroup(List<AccumulatorCache> caches, MarkerLinkageVo linkageVo) {
         final LinkRefDto linkRef = MarkerLinkageDataHelper.getLinkRef(linkageVo);
         AccumulatorCache ac = null;
 
