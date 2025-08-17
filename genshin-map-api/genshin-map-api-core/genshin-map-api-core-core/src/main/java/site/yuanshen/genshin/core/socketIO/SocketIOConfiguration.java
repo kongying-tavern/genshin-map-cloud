e2dfa7698774084f1bb7aca38a16f0f7db9039e8
Alex Fang <deeno@qq.com>
2025-08-17 08:17:55 +0800

    fix(netty): 修复SocketIO netty依赖

--- genshin-map-api/genshin-map-api-core/genshin-map-api-core-core/src/main/java/site/yuanshen/genshin/core/socketIO/SocketIOConfiguration.java
+++ genshin-map-api/genshin-map-api-core/genshin-map-api-core-core/src/main/java/site/yuanshen/genshin/core/socketIO/SocketIOConfiguration.java
@@ -5,6 +5,7 @@ import com.corundumstudio.socketio.annotation.SpringAnnotationScanner;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 
 
 @Configuration
