b80e75e5ccfd77423b849fa104a12c60f9e1c081
Alex Fang <deeno@qq.com>
2025-08-22 15:06:47 +0800

    refactor(SocketIOProperties): 分离SocketIO配置项

--- genshin-map-api/genshin-map-api-core/genshin-map-api-core-core/src/main/java/site/yuanshen/genshin/core/socketIO/SocketIOConfiguration.java
+++ genshin-map-api/genshin-map-api-core/genshin-map-api-core-core/src/main/java/site/yuanshen/genshin/core/socketIO/SocketIOConfiguration.java
@@ -2,66 +2,41 @@ package site.yuanshen.genshin.core.socketIO;
 
 import com.corundumstudio.socketio.SocketIOServer;
 import com.corundumstudio.socketio.annotation.SpringAnnotationScanner;
-import org.springframework.beans.factory.annotation.Value;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Lazy;
 
 
 @Configuration
+@RequiredArgsConstructor
 public class SocketIOConfiguration {
-
-    @Value("${socketio.host}")
-    private String host;
-
-    @Value("${socketio.port}")
-    private Integer port;
-
-    @Value("${socketio.worker-threads}")
-    private int workerThreads;
-
-    @Value("${socketio.allow-custom-requests}")
-    private boolean allowCustomRequests;
-
-    @Value("${socketio.upgrade-timeout}")
-    private int upgradeTimeout;
-
-    @Value("${socketio.ping-timeout}")
-    private int pingTimeout;
-
-    @Value("${socketio.ping-interval}")
-    private int pingInterval;
-
-    @Value("${socketio.max-frame-payload-length}")
-    private int maxFramePayloadLength;
-
-    @Value("${socketio.max-http-content-length}")
-    private int maxHttpContentLength;
+    private final SocketIOProperties properties;
 
     @Bean
     public SocketIOServer socketIOServer() {
         com.corundumstudio.socketio.Configuration config = new com.corundumstudio.socketio.Configuration();
         // 配置域名和端口
-        config.setHostname(host);
-        config.setPort(port);
+        config.setHostname(properties.getHost());
+        config.setPort(properties.getPort());
+        config.setContext("/ws");
         // 开启socket端口复用
         com.corundumstudio.socketio.SocketConfig socketConfig = new com.corundumstudio.socketio.SocketConfig();
         socketConfig.setReuseAddress(Boolean.TRUE);
         config.setSocketConfig(socketConfig);
         // 连接数大小
-        config.setWorkerThreads(workerThreads);
+        config.setWorkerThreads(properties.getWorkerThreads());
         // 允许客户请求
-        config.setAllowCustomRequests(allowCustomRequests);
+        config.setAllowCustomRequests(properties.isAllowCustomRequests());
         // 协议升级超时时间(毫秒)，默认10秒，HTTP握手升级为ws协议超时时间
-        config.setUpgradeTimeout(upgradeTimeout);
+        config.setUpgradeTimeout(properties.getUpgradeTimeout());
         // Ping消息超时时间(毫秒)，默认60秒，这个时间间隔内没有接收到心跳消息就会发送超时事件
-        config.setPingTimeout(pingTimeout);
+        config.setPingTimeout(properties.getPingTimeout());
         // Ping消息间隔(毫秒)，默认25秒。客户端向服务器发送一条心跳消息间隔
-        config.setPingInterval(pingInterval);
+        config.setPingInterval(properties.getPingInterval());
         // 设置HTTP交互最大内容长度
-        config.setMaxFramePayloadLength(maxFramePayloadLength);
+        config.setMaxFramePayloadLength(properties.getMaxFramePayloadLength());
         // 设置最大每帧处理数据的长度，防止他人利用大数据来攻击服务器
-        config.setMaxHttpContentLength(maxHttpContentLength);
+        config.setMaxHttpContentLength(properties.getMaxHttpContentLength());
 
         return new SocketIOServer(config);
     }
