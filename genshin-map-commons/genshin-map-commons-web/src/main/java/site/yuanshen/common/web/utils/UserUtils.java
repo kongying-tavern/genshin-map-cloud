aeeb060ad8bee7abbb013ad3c3df4cb49bf2ddae
Alex Fang <deeno@qq.com>
2025-09-07 11:56:31 +0800

    feat(UserUtils): 添加用户数据权限层级方法

--- genshin-map-commons/genshin-map-commons-web/src/main/java/site/yuanshen/common/web/utils/UserUtils.java
+++ genshin-map-commons/genshin-map-commons-web/src/main/java/site/yuanshen/common/web/utils/UserUtils.java
@@ -1,17 +1,31 @@
 package site.yuanshen.common.web.utils;
 
+import cn.hutool.core.util.StrUtil;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
 public class UserUtils {
     public static Long getUserId() {
         try {
-            ServletRequestAttributes requestAttributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
-            if (requestAttributes == null) return null;
-            String userId = requestAttributes.getRequest().getHeader("userId");
+            final ServletRequestAttributes requestAttributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+            if (requestAttributes == null)
+                return null;
+            final String userId = requestAttributes.getRequest().getHeader("userId");
             return Long.valueOf(userId);
         } catch (Exception e) {
             return null;
+        }
+    }
+
+    public static String getUserDataLevel() {
+        try {
+            final ServletRequestAttributes requestAttributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+            if (requestAttributes == null)
+                return "";
+            final String userDataLevel = requestAttributes.getRequest().getHeader("userDataLevel");
+            return StrUtil.blankToDefault(userDataLevel, "");
+        } catch (Exception e) {
+            return "";
         }
     }
 }
