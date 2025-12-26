7dd4a016bf7d2a7ad8324a1537a44d09fc273fe1
Alex Fang <deeno@qq.com>
2025-12-26 23:22:48 +0800

    fix(MarkerExtra): 更新接口字段名

--- genshin-map-data/genshin-map-data-core/genshin-map-data-core-model/src/main/java/site/yuanshen/data/vo/adapter/marker/marker/MarkerExtraVo.java
+++ genshin-map-data/genshin-map-data-core/genshin-map-data-core-model/src/main/java/site/yuanshen/data/vo/adapter/marker/marker/MarkerExtraVo.java
@@ -2,7 +2,9 @@ package site.yuanshen.data.vo.adapter.marker.marker;
 
 import com.alibaba.fastjson2.JSONWriter;
 import com.alibaba.fastjson2.annotation.JSONField;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
@@ -18,14 +20,17 @@ public class MarkerExtraVo {
     static class Underground {
         @Schema(title = "是否是分层层级")
         @JSONField(name = "is_underground")
+        @JsonProperty("is_underground")
         private Boolean isUnderground;
 
         @Schema(title = "是否是非地面层级")
         @JSONField(name = "is_global")
+        @JsonProperty("is_global")
         private Boolean isGlobal;
 
         @Schema(title = "分层区域", description = "分层区域标签")
         @JSONField(name = "region_levels")
+        @JsonProperty("region_levels")
         private List<String> regionLevels;
     }
 
@@ -33,14 +38,20 @@ public class MarkerExtraVo {
     @JsonInclude(JsonInclude.Include.NON_NULL)
     static class IconOverride {
         @Schema(title = "图标ID")
+        @JSONField(name = "id")
+        @JsonProperty("id")
         private Long id;
 
         @Schema(title = "最小可见缩放级别", description = "最小可见缩放级别，大于该值可见")
-        @JSONField(serializeFeatures = JSONWriter.Feature.WriteBigDecimalAsPlain)
+        @JSONField(name = "minZoom", serializeFeatures = JSONWriter.Feature.WriteBigDecimalAsPlain)
+        @JsonProperty("minZoom")
+        @JsonFormat(shape = JsonFormat.Shape.NUMBER_FLOAT)
         private BigDecimal minZoom;
 
         @Schema(title = "最大可见缩放级别", description = "最大可见缩放级别，小于该值可见")
-        @JSONField(serializeFeatures = JSONWriter.Feature.WriteBigDecimalAsPlain)
+        @JSONField(name = "maxZoom", serializeFeatures = JSONWriter.Feature.WriteBigDecimalAsPlain)
+        @JsonProperty("maxZoom")
+        @JsonFormat(shape = JsonFormat.Shape.NUMBER_FLOAT)
         private BigDecimal maxZoom;
     }
 
@@ -49,26 +60,32 @@ public class MarkerExtraVo {
     static class V2_8_Island {
         @Schema(title = "海岛名")
         @JSONField(name = "island_name")
+        @JsonProperty("island_name")
         private String islandName;
 
         @Schema(title = "海岛状态", description = "海岛状态标签")
         @JSONField(name = "island_state")
+        @JsonProperty("island_state")
         private List<String> islandState;
     }
 
     @Schema(title = "分层层级数据")
     @JSONField(name = "underground")
+    @JsonProperty("underground")
     private Underground underground;
 
     @Schema(title = "图标覆盖数据")
-    @JSONField(name = "icon_override")
+    @JSONField(name = "iconOverride")
+    @JsonProperty("iconOverride")
     private IconOverride iconOverride;
 
     @Schema(title = "1.6 海岛数据", description = "海岛阶段数组")
     @JSONField(name = "1_6_island")
+    @JsonProperty("1_6_island")
     private List<String> v1_6_Island;
 
     @Schema(title = "2.8 海岛数据")
     @JSONField(name = "2_8_island")
+    @JsonProperty("2_8_island")
     private V2_8_Island v2_8_Island;
 }
